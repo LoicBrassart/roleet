2c618bfc17a743d929510fe7f0119287312154d6
LoicBrassart <loic.brassart@gmail.com>
2024-10-18 10:11:01 +0200

    fix: fix a fcking error in Dockerfile caused by twind conf file being a .js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
+import "./globals.css";
 
 function App() {
   return (
-    <>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main>
+      <Outlet />
+    </main>
   );
 }
 
--- frontend/src/organisms/ScenarioDetail.tsx
+++ frontend/src/organisms/ScenarioDetail.tsx
@@ -1,3 +1,4 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/lib/shadcn/ui/tabs";
 import type { Scenario } from "../lib/graphql/generated/graphql-types";
 
 type Props = {
@@ -8,6 +9,22 @@ export default function ScenarioDetail({ data }: Props) {
     <>
       <h2>{data.title}</h2>
       <p>More coming Soon(tm)</p>
+      <Tabs defaultValue="home" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="home">Home</TabsTrigger>
+          <TabsTrigger value="plans">Plans</TabsTrigger>
+          <TabsTrigger value="flashcards">FlashCards</TabsTrigger>
+        </TabsList>
+        <TabsContent value="home">
+          <p>Home</p>
+        </TabsContent>
+        <TabsContent value="plans">
+          <p>Plans</p>
+        </TabsContent>
+        <TabsContent value="flashcards">
+          <p>FlashCards</p>
+        </TabsContent>
+      </Tabs>
     </>
   );
 }
