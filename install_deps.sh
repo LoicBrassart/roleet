c1708ed86d94c8baaf07728782b0d6add6cefee2
Forthtilliath <forth@live.fr>
2025-04-16 00:36:46 +0200

    ci: remake script npm i all

--- install_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exécutez npm install dans chaque dossier spécifié en argument
-for dossier in "$@"; do
-  echo "Installation des dépendances dans $dossier..."
-  cd "$dossier" && npm install && cd - > /dev/null
-done
