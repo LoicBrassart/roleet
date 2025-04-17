fd42c757ebbc06a86159c4dc92400d7540f09de8
Forthtilliath <forth@live.fr>
2025-04-17 18:28:41 +0200

    chore: simplify component + resolve types 🌪

--- frontend/src/organisms/Campaign/CampaignForm.tsx
+++ frontend/src/organisms/Campaign/CampaignForm.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/atoms/Button";
 import { EditableField } from "@/atoms/EditableField";
-import { FormMessage } from "@/atoms/FormMessage";
 import { Select } from "@/atoms/Select";
 import {
   useCreateCampaignMutation,
@@ -8,31 +7,29 @@ import {
   useGetMyScenariosQuery,
 } from "@/lib/graphql/generated/graphql-types";
 import { getOptions } from "@/lib/helpers/forms";
-import type { Option } from "@/lib/helpers/zodSchemas";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "@/lib/shadcn/generated/ui/form";
-import { useUserStore } from "@/lib/zustand/userStore";
+  type CreateCampainOutput,
+  createCampaignSchema,
+} from "@/lib/schemas/campaignCreate";
+import { updateCampaignSchema } from "@/lib/schemas/campaignUpdate";
+import type { Option } from "@/lib/schemas/formOptions";
+import { Form } from "@/lib/shadcn/generated/ui/form";
 import type { Q } from "@/types/queries";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { type SubmitHandler, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { z } from "zod";
 
 type Props = {
   campaign?: Q.MyCampaign;
 };
 export default function CampaignForm({ campaign }: Props) {
-  const user = useUserStore((state) => state.user);
   const [createCampaign] = useCreateCampaignMutation();
+  // const [updateCampaign] = useUpdateCampaignMutation();
   const navigate = useNavigate();
+  const stateForm = campaign?.id ? "update" : "create";
 
-  if (!user) return <p>User not found</p>;
+  const { data: scenData } = useGetMyScenariosQuery();
+  const { data: usersData } = useGetAllUsersQuery();
 
   let defaultPlayers: Option[] = [];
   let defaultScenarios: Option[] = [];
@@ -41,109 +38,63 @@ export default function CampaignForm({ campaign }: Props) {
     defaultScenarios = getOptions(campaign.scenarios, "id", "title");
   }
 
-  const campaignSchema = z.object({
-    title: z
-      .string()
-      .min(4, {
-        message: "doit contenir au moins 4 caractères.",
-      })
-      .max(64, {
-        message: "doit contenir au maximum 64 caractères.",
-      }),
-    bannerUrl: z
-      .string()
-      .min(4, {
-        message: "doit contenir au moins 4 caractères.",
-      })
-      .max(256, {
-        message: "doit contenir au maximum 256 caractères.",
-      }),
-    players: z.array(z.string()),
-    scenarios: z.array(z.string()),
-  });
-
-  const { data: scenData } = useGetMyScenariosQuery();
-  const { data: usersData } = useGetAllUsersQuery();
-
-  const form = useForm<z.infer<typeof campaignSchema>>({
-    resolver: zodResolver(campaignSchema),
+  const form = useForm({
+    resolver: zodResolver(
+      stateForm === "create" ? createCampaignSchema : updateCampaignSchema,
+    ),
     defaultValues: {
       title: campaign?.title ?? "",
       bannerUrl: campaign?.bannerUrl ?? "",
+      players: defaultPlayers as unknown as string[],
+      scenarios: defaultScenarios as unknown as string[],
     },
   });
 
+  const { handleSubmit, register } = form;
+
   // TODO: Fix bannerUrl : mandatory or optional ?
-  const handleSubmit = () => {
-    return form.handleSubmit(async (values) => {
-      const { data } = await createCampaign({ variables: { data: values } });
-      if (!data) return;
-      const campId = data.createCampaign.id;
-      navigate(`/campaign/${campId}`);
-    });
+  const onSubmit: SubmitHandler<CreateCampainOutput> = async (values) => {
+    if (campaign?.id) {
+      // const updated = await updateCampaign({
+      //   variables: { id: campaign.id, data: values },
+      // });
+      // navigate(`/campaign/${campId}`);
+      return;
+    }
+
+    const { data } = await createCampaign({ variables: { data: values } });
+    if (!data) return;
+    const campId = data.createCampaign.id;
+    navigate(`/campaign/${campId}`);
   };
 
   return (
     <Form {...form}>
-      <form onSubmit={handleSubmit} className="space-y-6" method="post">
-        <EditableField label="Titre" name="title" control={form.control} />
-        <EditableField
-          label="Bannière"
-          name="bannerUrl"
-          control={form.control}
-        />
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <EditableField label="Titre" {...register("title")} />
+
+        <EditableField label="Bannière" {...register("bannerUrl")} />
+
         {usersData && (
-          <FormField
-            control={form.control}
-            name="players"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Joueurs</FormLabel>
-                <FormControl>
-                  <Select
-                    {...field}
-                    label="Joueurs"
-                    name="players"
-                    control={form.control}
-                    isMulti
-                    options={getOptions(usersData.getAllUsers, "id", "name")}
-                    defaultValue={defaultPlayers}
-                  />
-                </FormControl>
-                <FormDescription>
-                  les joueurs que vous souhaitez voir venir sur cette tablée
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+          <Select
+            label="Joueurs"
+            description="les joueurs que vous souhaitez voir venir sur cette tablée"
+            isMulti
+            options={getOptions(usersData.getAllUsers, "id", "name")}
+            {...register("players")}
           />
         )}
+
         {scenData && (
-          <FormField
-            control={form.control}
-            name="scenarios"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Scenarios</FormLabel>
-                <FormControl>
-                  <Select
-                    {...field}
-                    label="Scenarios"
-                    name="scenarios"
-                    control={form.control}
-                    isMulti
-                    options={getOptions(scenData.getMyScenarios, "id", "title")}
-                    defaultValue={defaultScenarios}
-                  />
-                </FormControl>
-                <FormDescription>
-                  les scenarios que vous voulez offrir à vos joueurs
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
+          <Select
+            label="scenarios"
+            description="les scenarios que vous voulez offrir à vos joueurs"
+            isMulti
+            options={getOptions(scenData.getMyScenarios, "id", "title")}
+            {...register("scenarios")}
           />
         )}
+
         <Button type="submit">Sauvegarder</Button>
       </form>
     </Form>
