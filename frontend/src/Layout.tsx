e0dbbf7af0ebdd87f15582c780b5f4a81a3cbf8d
Forthtilliath <forth@live.fr>
2025-05-24 05:26:18 +0200

    chore: add settings on sidebar

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,5 +1,6 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./globals.css";
+import { useEffect, useState } from "react";
 import AppSidebar from "./layout/AppSidebar";
 import {
   SidebarProvider,
@@ -7,12 +8,24 @@ import {
 } from "./lib/shadcn/generated/ui/sidebar";
 
 export default function Layout() {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Deps are fine
+  useEffect(() => {
+    setOpen(false);
+
+    return () => {
+      setOpen(false);
+    };
+  }, [location.pathname]);
+
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={false} open={open} onOpenChange={setOpen}>
       <div className="absolute">
         <AppSidebar />
       </div>
-      <main className="w-8/12 dark m-auto mt-0">
+      <main className="dark m-auto mt-0 w-8/12">
         <SidebarTrigger />
         <Outlet />
       </main>
