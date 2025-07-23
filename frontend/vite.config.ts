777c2ca6794b4dae07fe930776c2361fa9933612
LoicBrassart <loic.brassart@gmail.com>
2025-07-23 15:27:57 +0200

    refactor: build a whole new frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,34 +1,15 @@
-import path from "node:path";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import type { ViteUserConfig } from "vitest/config";
 
-// https://vitejs.dev/config/
-const config: ViteUserConfig = {
-  plugins: [react(), tailwindcss()],
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: true,
-    watch: {
-      usePolling: true,
-    },
+    allowedHosts: true,
     hmr: {
       port: 7000,
       path: "/hmr",
     },
-    allowedHosts: true,
   },
-  build: {
-    outDir: "./build",
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-};
-export default defineConfig(config);
+});
