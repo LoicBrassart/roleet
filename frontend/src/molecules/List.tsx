b53889ede04538db2f9e1a7280a04de13fdf8dae
LoicBrassart <loic.brassart@gmail.com>
2025-08-21 22:52:29 +0200

    refactor: biome happens

--- frontend/src/molecules/List.tsx
+++ frontend/src/molecules/List.tsx
@@ -13,7 +13,7 @@ export function List<T>({ title, data, render }: Props<T>) {
       <ul className="space-y-4">
         {data.map((item, index) => {
           // TODO: trouver une meilleure solution pour la key. Hint: message de Forth le 28/07/25 à 22:11
-          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+          // biome-ignore lint/suspicious/noArrayIndexKey: Not optimal, but since this component is not meant to be used with rapidly-updated lists it shouldn't be a problem
           return <li key={index}>{render(item)}</li>;
         })}
       </ul>
