9a7c73fcb8edb168a10329bf93fbf875e9a7b210
LoicBrassart <loic.brassart@gmail.com>
2025-09-04 22:02:40 +0200

    feat: improve login's UX

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,11 +1,12 @@
-import { Outlet, useNavigate } from "react-router";
+import { Link, Outlet, useNavigate } from "react-router";
 import { useLogoutMutation } from "@/lib/graphql/generated/graphql-types";
-import { useLogout } from "@/lib/zustand/userStore";
+import { useCurrentUser, useLogout } from "@/lib/zustand/userStore";
 
 export default function Layout() {
   const [logout] = useLogoutMutation();
   const logoutFromStore = useLogout();
   const navigate = useNavigate();
+  const user = useCurrentUser();
 
   const hLogout = () => {
     logout();
@@ -15,14 +16,36 @@ export default function Layout() {
 
   return (
     <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main className="mb-20 w-dvw overflow-x-hidden p-4">
         <Outlet />
       </main>
       <footer className="fixed right-0 bottom-0 left-0 bg-black/40 p-4">
         <h2>DEBUG ZONE</h2>
-        <button type="button" onClick={hLogout}>
-          Logout
-        </button>
+        {user ? (
+          <>
+            <button type="button" onClick={hLogout}>
+              Logout
+            </button>
+            {/* <div>
+              <p>You're logged in as {user.name}</p>
+              <pre>{JSON.stringify(user, null, 4)}</pre>
+            </div> */}
+          </>
+        ) : (
+          <Link to="/auth">Authentification</Link>
+        )}
       </footer>
     </>
   );
